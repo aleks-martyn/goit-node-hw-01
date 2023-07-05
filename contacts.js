351328d6e42e83c36e915fb4267ad0d9c3ef98c6
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-05 23:32:04 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -4,6 +4,22 @@ const path = require("path");
 const contactsPath = path.join("./db", "contacts.json");
 
 function listContacts() {
-    fs.readFile(contactsPath)
-    .then(data => {return JSON.parse(data)})
+  fs.readFile(contactsPath)
+    .then((data) => {
+      return JSON.parse(data);
+    })
+    .then((result) => console.table(result))
+    .catch((error) => console.log(error.message));
 }
+
+const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath);
+  const result = JSON.parse(data);
+  const contactById = result.filter((contact) => contact.id === contactId);
+  return console.table(contactById);
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+};
