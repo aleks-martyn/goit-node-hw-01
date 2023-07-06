ae9127daf99cb546fc274ac69e1edfeab85e067f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-07 00:02:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+const { Command } = require("commander");
 const {
   listContacts,
   getContactById,
   removeContact,
   addContact,
 } = require("./contacts.js");
+
+const program = new Command();
+
+program
+  .option(
+    "-a, --action <string>",
+    "choose action: list, get -i, add -n -e -p, remove -i"
+  )
+  .option("-i, --id <string>", "user id")
+  .option("-n, --name <string>", "user name")
+  .option("-e, --email <string>", "user email")
+  .option("-p, --phone <string>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
